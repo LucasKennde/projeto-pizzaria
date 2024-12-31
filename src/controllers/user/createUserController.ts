29cc343254b4bed58df1788328cc62e68ddec5dd
Lucas Kennde <lucaskennde@gmail.com>
2024-12-31 18:17:59 -0300

    Update version @types/express

--- src/controllers/user/createUserController.ts
+++ src/controllers/user/createUserController.ts
@@ -2,15 +2,20 @@ import {Response, Request} from 'express'
 import { CreateUserService } from '../../services/users/createUserService';
 export class CreateUserController{
 
-    async handle(req:Request, res:Response){
-        const {name, email, password} = req.body;
-        const createUserService = new CreateUserService();
-
-        const user = await createUserService.execute({
-            name,
-            email,
-            password}
-        );
-        return res.json(user)
+    async handle(req:Request, res:Response):Promise<Response>{
+        try {
+            const { name, email, password } = req.body;
+            const createUserService = new CreateUserService();
+            const user = await createUserService.execute({
+              name,
+              email,
+              password,
+            });
+        
+            return res.status(201).json(user); 
+          } catch (error) {
+            console.error(error);
+            return res.status(500).json({ error: 'Internal Server Error' });
+          }
     }
 }
